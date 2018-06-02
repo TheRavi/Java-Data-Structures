e05abdde8aad6a349415bfc94f37d84fc498873f
TheRavi <ravi.it.2015@gmail.com>
2018-06-02 17:51:12 +0530

    implementing push(item) operation.

--- com/problems/stack/StackArray.java
+++ com/problems/stack/StackArray.java
@@ -8,11 +8,20 @@ package com.problems.stack;
  *
  */
 public class StackArray {
+  static int top = -1;
+  static int[] numArray = new int[5];
 
   /**
    * @param args
    */
   public static void main(String[] args) {
+    push(numArray,3);
+    push(numArray,4);
+  }
 
+  public static int[] push(int[] stack, int item) {
+    top++;
+    numArray[top] = item;
+    return stack;
   }
 }
